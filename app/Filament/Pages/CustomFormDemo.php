f39a413cf0f05138046aa2472cc5704f65493626
heloufir <eloufirhatim@gmail.com>
2023-12-02 23:02:43 -0500

    Custom form

--- app/Filament/Pages/CustomFormDemo.php
+++ app/Filament/Pages/CustomFormDemo.php
@@ -45,7 +45,7 @@ class CustomFormDemo extends Kanban
                 ->color('gray')
                 ->icon('heroicon-m-code-bracket')
                 ->label('View on Github')
-                ->url('https://github.com/heloufir/filament-kanban-demo/blob/main/BasicDemo.php')
+                ->url('https://github.com/heloufir/filament-kanban-demo/blob/main/CustomFormDemo.php')
         ], Parent::getActions());
     }
 
