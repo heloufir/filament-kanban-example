9d5543d87f8abd2923e6040d6af08a0d459bb962
heloufir <eloufirhatim@gmail.com>
2023-12-11 15:31:54 -0500

    Add status icon support

--- packages/heloufir/filament-kanban/resources/views/livewire/partials/column-header.blade.php
+++ packages/heloufir/filament-kanban/resources/views/livewire/partials/column-header.blade.php
@@ -1,7 +1,15 @@
 <div
     class="kanban-col-title flex flex-row items-center gap-2 sticky top-0 bg-slate-100 dark:bg-slate-800 p-3 z-10">
-    <div class="kanban-col-title-color w-3 h-3 rounded-full border-2"
-         style="border-color: {{ $status['color'] ?? 'gray' }};"></div>
+    @if(isset($status['icon']))
+        <x-filament::icon
+            icon="{{$status['icon']}}"
+            class="h-4 w-4"
+            style="color: {{ $status['color'] ?? 'gray' }};"
+        />
+    @else
+        <div class="kanban-col-title-color w-3 h-3 rounded-full border-2"
+             style="border-color: {{ $status['color'] ?? 'gray' }};"></div>
+    @endif
     <span
         class="kanban-col-title-status text-sm font-medium text-gray-700 dark:text-gray-300">{{ $status['name'] }}</span>
     <span
